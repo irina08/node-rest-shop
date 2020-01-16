252ed4615d4d7258a026b8f3e268515f4eaf8902
irina08 <irina_ger@yahoo.com>
2020-01-15 19:36:31 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/*const http = require('http') ;
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer();
+
+server.listen(port);
+*/
+/*
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('An alligator approaches!');
+});
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+*/
